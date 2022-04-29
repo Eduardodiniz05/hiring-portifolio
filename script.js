ae45eb685c4d825fac4abe4cfd1cfb766cb3ae94
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-04-29 10:12:24 -0300

    iniciado parte do JS

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayAbout() {
+    let arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+    let display = document.getElementById('about') ;
+        display.classList.toggle('active');
+    
+}
+
